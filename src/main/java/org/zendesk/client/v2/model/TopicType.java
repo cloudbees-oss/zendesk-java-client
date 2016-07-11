29ba94059131a396c266f5fac27e9e3917f1e050
Paw Suddergaard <paw@falconsocial.com>
2016-07-11 14:18:08 +0200

    Topic types are lowecased
    Reference: https://developer.zendesk.com/rest_api/docs/web-portal/forums

--- src/main/java/org/zendesk/client/v2/model/TopicType.java
+++ src/main/java/org/zendesk/client/v2/model/TopicType.java
@@ -1,9 +1,9 @@
 package org.zendesk.client.v2.model;
 
 public enum TopicType {
-    ARTICLES("Articles"),
-    QUESTIONS("Questions"),
-    IDEAS("Ideas");
+    ARTICLES("articles"),
+    QUESTIONS("questions"),
+    IDEAS("ideas");
 
     private final String name;
 
