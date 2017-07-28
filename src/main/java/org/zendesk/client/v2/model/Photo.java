c6a7d98c1d0f2df7deaf2a73128a20871a17d001
Eric O'Connell <eric@roundpegdesigns.com>
2017-07-28 14:23:40 -0700

    Add additional attributes for Photo found in API responses, add JsonIgnoreProperties.ignoreUnknown (#185)

--- src/main/java/org/zendesk/client/v2/model/Photo.java
+++ src/main/java/org/zendesk/client/v2/model/Photo.java
@@ -2,21 +2,28 @@ package org.zendesk.client.v2.model;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author stephenc
  * @since 05/04/2013 15:36
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Photo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
     private String fileName;
+    private String url;
     private String contentUrl;
+    private String mappedContentUrl;
     private String contentType;
     private Long size;
+    private Long width;
+    private Long height;
+    private Boolean inline;
 
     @JsonProperty("content_type")
     public String getContentType() {
@@ -27,6 +34,14 @@ public class Photo implements Serializable {
         this.contentType = contentType;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     @JsonProperty("content_url")
     public String getContentUrl() {
         return contentUrl;
@@ -34,6 +49,15 @@ public class Photo implements Serializable {
 
     public void setContentUrl(String contentUrl) {
         this.contentUrl = contentUrl;
+    }
+
+    @JsonProperty("mapped_content_url")
+    public String getMappedContentUrl() {
+        return mappedContentUrl;
+    }
+
+    public void setMappedContentUrl(String mappedContentUrl) {
+        this.mappedContentUrl = mappedContentUrl;
     }
 
     @JsonProperty("file_name")
@@ -61,6 +85,30 @@ public class Photo implements Serializable {
         this.size = size;
     }
 
+    public Long getWidth() {
+        return width;
+    }
+
+    public void setWidth(Long width) {
+        this.width = width;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public Boolean getInline() {
+        return inline;
+    }
+
+    public void setInline(Boolean inline) {
+        this.inline = inline;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
@@ -70,6 +118,9 @@ public class Photo implements Serializable {
         sb.append(", fileName='").append(fileName).append('\'');
         sb.append(", contentUrl='").append(contentUrl).append('\'');
         sb.append(", size=").append(size);
+        sb.append(", width=").append(width);
+        sb.append(", height=").append(height);
+        sb.append(", inline=").append(inline);
         sb.append('}');
         return sb.toString();
     }
