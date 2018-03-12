e01557dab9863a6489bee3faafaef6467ec507ae
tkurzawa <tzkurzawa@gmail.com>
2018-03-12 09:59:23 +0100

    Added hashCode and equals methods to the CustomFieldValue class.

--- src/main/java/org/zendesk/client/v2/model/CustomFieldValue.java
+++ src/main/java/org/zendesk/client/v2/model/CustomFieldValue.java
@@ -52,4 +52,32 @@ public class CustomFieldValue implements Serializable {
                 ", value=" + Arrays.toString(value) +
                 '}';
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.hashCode(value);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomFieldValue other = (CustomFieldValue) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.equals(value, other.value))
+			return false;
+		return true;
+	}
 }
