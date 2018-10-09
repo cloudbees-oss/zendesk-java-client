e445066b2c2de8ce36f02501decfaa6f8aaeb678
Henrique Cabral <hcabral@bureau.works>
2018-10-09 10:17:00 -0400

    Added Name and Description attributes to Translation class

--- src/main/java/org/zendesk/client/v2/model/hc/Translation.java
+++ src/main/java/org/zendesk/client/v2/model/hc/Translation.java
@@ -5,183 +5,198 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Translation {
-  /* Automatically assigned when a translation is created */
-  private Long id;
+    /* Automatically assigned when a translation is created */
+    private Long id;
 
-  /* The API url of the translation */
-  private String url;
+    /* The API url of the translation */
+    private String url;
 
-  /* The url of the translation in Help Center */
-  @JsonProperty("html_url")
-  private String htmlUrl;
+    /* The url of the translation in Help Center */
+    @JsonProperty("html_url")
+    private String htmlUrl;
 
-  /* The id of the item that has this translation */
-  @JsonProperty("source_id")
-  private Long sourceId;
+    /* The id of the item that has this translation */
+    @JsonProperty("source_id")
+    private Long sourceId;
 
-  /* The type of the item that has this translation. Can be Article, Section, orCategory */
-  @JsonProperty("source_type")
-  private String sourceType;
+    /*
+     * The type of the item that has this translation. Can be Article, Section,
+     * orCategory
+     */
+    @JsonProperty("source_type")
+    private String sourceType;
 
-  /* The locale of the translation */
-  private String locale;
+    /* The locale of the translation */
+    private String locale;
 
-  /* The title of the translation */
-  private String title;
+    /* The name of the Section or Category */
+    private String name;
 
-  /* The body of the translation. Empty by default */
-  private String body;
+    /* The description of the Section or Category */
+    private String description;
 
-  /* True if the translation is outdated; false otherwise. False by default */
-  private boolean outdated;
+    /* The title of the translation */
+    private String title;
 
-  /* True if the translation is a draft; false otherwise. False by default */
-  private boolean draft;
+    /* The body of the translation. Empty by default */
+    private String body;
 
-  /* The time at which the translation was created */
-  @JsonProperty("created_at")
-  private Date createdAt;
+    /* True if the translation is outdated; false otherwise. False by default */
+    private boolean outdated;
 
-  /* The time at which the translation was last updated */
-  @JsonProperty("updated_at")
-  private Date updatedAt;
+    /* True if the translation is a draft; false otherwise. False by default */
+    private boolean draft;
 
-  /* The id of the user who last updated the translation */
-  @JsonProperty("updated_by_id")
-  private Long updatedById;
+    /* The time at which the translation was created */
+    @JsonProperty("created_at")
+    private Date createdAt;
 
-  /* The id of the user who created the translation */
-  @JsonProperty("created_by_id")
-  private Long createdById;
+    /* The time at which the translation was last updated */
+    @JsonProperty("updated_at")
+    private Date updatedAt;
 
-  public Long getId() {
-      return id;
-  }
+    /* The id of the user who last updated the translation */
+    @JsonProperty("updated_by_id")
+    private Long updatedById;
 
-  public void setId( Long id ) {
-      this.id = id;
-  }
+    /* The id of the user who created the translation */
+    @JsonProperty("created_by_id")
+    private Long createdById;
 
-  public String getUrl() {
-      return url;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setUrl( String url ) {
-      this.url = url;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public String getHtmlUrl() {
-      return htmlUrl;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public void setHtmlUrl( String htmlUrl ) {
-      this.htmlUrl = htmlUrl;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public Long getSourceId() {
-      return sourceId;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setSourceId( Long sourceId ) {
-      this.sourceId = sourceId;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getSourceType() {
-      return sourceType;
-  }
+    public String getUrl() {
+        return url;
+    }
 
-  public void setSourceType( String sourceType ) {
-      this.sourceType = sourceType;
-  }
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-  public String getLocale() {
-      return locale;
-  }
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
 
-  public void setLocale( String locale ) {
-      this.locale = locale;
-  }
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
 
-  public String getTitle() {
-      return title;
-  }
+    public Long getSourceId() {
+        return sourceId;
+    }
 
-  public void setTitle( String title ) {
-      this.title = title;
-  }
+    public void setSourceId(Long sourceId) {
+        this.sourceId = sourceId;
+    }
 
-  public String getBody() {
-      return body;
-  }
+    public String getSourceType() {
+        return sourceType;
+    }
 
-  public void setBody( String body ) {
-      this.body = body;
-  }
+    public void setSourceType(String sourceType) {
+        this.sourceType = sourceType;
+    }
 
-  public boolean isOutdated() {
-      return outdated;
-  }
+    public String getLocale() {
+        return locale;
+    }
 
-  public void setOutdated( boolean outdated ) {
-      this.outdated = outdated;
-  }
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
 
-  public boolean isDraft() {
-      return draft;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public void setDraft( boolean draft ) {
-      this.draft = draft;
-  }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-  public Date getCreatedAt() {
-      return createdAt;
-  }
+    public String getBody() {
+        return body;
+    }
 
-  public void setCreatedAt( Date createdAt ) {
-      this.createdAt = createdAt;
-  }
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-  public Date getUpdatedAt() {
-      return updatedAt;
-  }
+    public boolean isOutdated() {
+        return outdated;
+    }
 
-  public void setUpdatedAt( Date updatedAt ) {
-      this.updatedAt = updatedAt;
-  }
+    public void setOutdated(boolean outdated) {
+        this.outdated = outdated;
+    }
 
-  public Long getUpdatedById() {
-      return updatedById;
-  }
+    public boolean isDraft() {
+        return draft;
+    }
 
-  public void setUpdatedById( Long updatedById ) {
-      this.updatedById = updatedById;
-  }
+    public void setDraft(boolean draft) {
+        this.draft = draft;
+    }
 
-  public Long getCreatedById() {
-      return createdById;
-  }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 
-  public void setCreatedById( Long createdById ) {
-      this.createdById = createdById;
-  }
-  @Override
-  public String toString() {
-      return "Translation{" +
-          "id=" + id + '\'' +
-          ", url=" + url + '\'' +
-          ", htmlUrl=" + htmlUrl + '\'' +
-          ", sourceId=" + sourceId + '\'' +
-          ", sourceType=" + sourceType + '\'' +
-          ", locale=" + locale + '\'' +
-          ", title=" + title + '\'' +
-          ", body=" + body + '\'' +
-          ", outdated=" + outdated + '\'' +
-          ", draft=" + draft + '\'' +
-          ", createdAt=" + createdAt + '\'' +
-          ", updatedAt=" + updatedAt + '\'' +
-          ", updatedById=" + updatedById + '\'' +
-          ", createdById=" + createdById + '\'' +
-          '}';
-  }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getUpdatedById() {
+        return updatedById;
+    }
+
+    public void setUpdatedById(Long updatedById) {
+        this.updatedById = updatedById;
+    }
+
+    public Long getCreatedById() {
+        return createdById;
+    }
+
+    public void setCreatedById(Long createdById) {
+        this.createdById = createdById;
+    }
+
+    @Override
+    public String toString() {
+        return "Translation{" + "id=" + id + '\'' + ", url=" + url + '\'' + ", htmlUrl=" + htmlUrl + '\''
+                + ", sourceId=" + sourceId + '\'' + ", sourceType=" + sourceType + '\'' + ", locale=" + locale + '\''
+                + ", title=" + title + '\'' + ", body=" + body + '\'' + ", outdated=" + outdated + '\'' + ", draft="
+                + draft + '\'' + ", createdAt=" + createdAt + '\'' + ", updatedAt=" + updatedAt + '\''
+                + ", updatedById=" + updatedById + '\'' + ", createdById=" + createdById + '\'' + '}';
+    }
 }
