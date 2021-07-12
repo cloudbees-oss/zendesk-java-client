2c607e3dd9484f42c4d90a93467ba8aa9372af97
Cott Lang <cott@possiblefinance.com>
2021-07-12 12:03:02 -0700

    Add end_user_conditions to ticket form

--- src/main/java/org/zendesk/client/v2/model/TicketForm.java
+++ src/main/java/org/zendesk/client/v2/model/TicketForm.java
@@ -32,6 +32,8 @@ public class TicketForm implements SearchResultEntity, Serializable {
     private Date createdAt;
 	@JsonProperty("updated_at")
     private Date updatedAt;
+	@JsonProperty("end_user_conditions")
+	private List<EndUserCondition> endUserConditions;
 
 	public String getName() {
 		return name;
@@ -104,5 +106,28 @@ public class TicketForm implements SearchResultEntity, Serializable {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public List<EndUserCondition> getEndUserConditions() {
+		return endUserConditions;
+	}
+
+	public void setEndUserConditions(List<EndUserCondition> endUserConditions) {
+		this.endUserConditions = endUserConditions;
+	}
+
+	public static class EndUserCondition implements Serializable {
+		private static final long serialVersionUID = 1L;
+		@JsonProperty("parent_field_id")
+		private String parentFieldId;
+		private String value;
+		@JsonProperty("child_fields")
+		private List<ChildField> childFields;
+	}
+
+	public static class ChildField implements Serializable {
+		private static final long serialVersionUID = 1L;
+		private String id;
+		private boolean isRequired;
 	}
 }
