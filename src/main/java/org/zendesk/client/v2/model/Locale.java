85dbdb8dbe6930c35aed9eaa34f5532f22ce6e95
Toni Pérez <antonio.perez@mimacom.com>
2023-10-26 08:13:20 +0200

    Revert "Add undocumented attributes to Locale"
    This reverts commit c98eaf2cc12028065e3d5e0781e85e8bf7abcfda.

--- src/main/java/org/zendesk/client/v2/model/Locale.java
+++ src/main/java/org/zendesk/client/v2/model/Locale.java
@@ -16,13 +16,9 @@ public class Locale implements Serializable {
   private static final long serialVersionUID = 1L;
 
   private Date createdAt;
-  private Boolean isDefault;
   private Long id;
   private String locale;
   private String name;
-  private String nativeName;
-  private String presentationName;
-  private Boolean rtl;
   private Date updatedAt;
   private String url;
 
@@ -33,14 +29,6 @@ public class Locale implements Serializable {
 
   public void setCreatedAt(Date createdAt) {
     this.createdAt = createdAt;
-  }
-
-  public Boolean isDefault() {
-    return isDefault;
-  }
-
-  public void setDefault(Boolean isDefault) {
-    this.isDefault = isDefault;
   }
 
   public Long getId() {
@@ -67,32 +55,6 @@ public class Locale implements Serializable {
     this.name = name;
   }
 
-  @JsonProperty("native_name")
-  public String getNativeName() {
-    return nativeName;
-  }
-
-  public void setNativeName(String nativeName) {
-    this.nativeName = nativeName;
-  }
-
-  @JsonProperty("presentation_name")
-  public String getPresentationName() {
-    return presentationName;
-  }
-
-  public void setPresentationName(String presentationName) {
-    this.presentationName = presentationName;
-  }
-
-  public Boolean getRtl() {
-    return rtl;
-  }
-
-  public void setRtl(Boolean rtl) {
-    this.rtl = rtl;
-  }
-
   @JsonProperty("updated_at")
   public Date getUpdatedAt() {
     return updatedAt;
@@ -115,8 +77,6 @@ public class Locale implements Serializable {
     return "Locale{"
         + "createdAt="
         + createdAt
-        + ", isDefault="
-        + isDefault
         + ", id="
         + id
         + ", locale='"
@@ -125,14 +85,6 @@ public class Locale implements Serializable {
         + ", name='"
         + name
         + '\''
-        + ", nativeName='"
-        + nativeName
-        + '\''
-        + ", presentationName='"
-        + presentationName
-        + '\''
-        + ", rtl="
-        + rtl
         + ", updatedAt="
         + updatedAt
         + ", url='"
