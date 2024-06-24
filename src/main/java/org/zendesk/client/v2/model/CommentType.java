dab77fd69cff69f76b3f95b3e7694329bea7ce03
Pierre Beitz <pibeitz@gmail.com>
2024-06-24 14:33:58 +0200

    [feature] Add support for the Talk Partner Edition type comment
    fixes #691

--- src/main/java/org/zendesk/client/v2/model/CommentType.java
+++ src/main/java/org/zendesk/client/v2/model/CommentType.java
@@ -3,7 +3,9 @@ package org.zendesk.client.v2.model;
 /** https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_comments/ */
 public enum CommentType {
   COMMENT("Comment"),
-  VOICE_COMMENT("VoiceComment");
+  VOICE_COMMENT("VoiceComment"),
+  // Talk Partner Edition
+  TPE_VOICE_COMMENT("TpeVoiceComment");
 
   private final String name;
 
