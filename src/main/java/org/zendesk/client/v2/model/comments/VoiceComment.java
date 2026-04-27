c57b160a5b702b5120e9cf5b484d035ae59efd88
Aleksei Averchenko <aleksei.averchenko@wise.com>
2026-04-27 17:29:29 +0100

    Fixed the `Comment` parsing errors when the `type` field is absent

--- src/main/java/org/zendesk/client/v2/model/comments/VoiceComment.java
+++ src/main/java/org/zendesk/client/v2/model/comments/VoiceComment.java
@@ -1,8 +1,10 @@
 package org.zendesk.client.v2.model.comments;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.zendesk.client.v2.model.Comment;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VoiceComment extends Comment {
 
   private VoiceCommentData data;
